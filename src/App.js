1eef9f5a9cfd75d46ef3294de068081bf5f72791
crowcrow07 <skathd3402@gmail.com>
2024-01-03 18:25:58 +0900

    feat : app.js 모달창 자리 확보 및 경로 변경

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import { useState } from "react";
 import Sidebar from "./components/sidebar/Sidebar";
 import ResizeDragZone from "./components/resizeDragZone/ResizeDragZone";
 
-import { DATA } from "./utils/DATA";
+import { initData } from "./utils/data";
 
 function App() {
-  const [data, setData] = useState(DATA);
+  const [data, setData] = useState(initData);
   const [clickedDiv, setClickedDiv] = useState(null);
+  const [isModalOpen, setIsModalOpen] = useState(true);
 
   const handleOpenButton = (sidebar) => {
     const isIdExist = data.some((item) => item.id === sidebar.id);
@@ -31,8 +32,32 @@ function App() {
     }
   };
 
+  // 모달을 열고 닫는 함수
+  // const handleOpenModal = () => {
+  //   setIsModalOpen(true);
+  // };
+
+  // const handleCloseModal = () => {
+  //   setIsModalOpen(false);
+  // };
+
   return (
-    <div className="flex h-screen">
+    <div className="relative flex h-screen">
+      {/* {isModalOpen && (
+        <div
+          className="fixed inset-0 z-50 bg-black opacity-50"
+          onClick={handleCloseModal}
+        ></div>
+      )}
+
+      <div className="absolute flex w-screen h-screen">
+        {isModalOpen && (
+          <div className="z-50 p-16 m-auto bg-white">
+            <div>모달창을 넣을 곳</div>
+            <button onClick={handleCloseModal}>닫기</button>
+          </div>
+        )}
+      </div> */}
       <Sidebar DATA={data} handleOpenButton={handleOpenButton} />
       <ResizeDragZone
         DATA={data}
