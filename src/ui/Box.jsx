06ab4586ef2e63abde80b5c11a90054c8259821a
crowcrow07 <skathd3402@gmail.com>
2024-01-03 16:56:35 +0900

    style : 공통 컴포넌트 박스 스타일 수정

--- src/ui/Box.jsx
+++ src/ui/Box.jsx
@@ -3,16 +3,8 @@ import React from "react";
 export default React.forwardRef(function Box(props, ref) {
   const { className, children, ...others } = props;
   return (
-    <div
-      {...others}
-      ref={ref}
-      className={`w-full h-full bg-white ${className}`}
-    >
+    <div {...others} ref={ref} className={`w-full bg-white ${className}`}>
       {children}
     </div>
   );
 });
-
-// w-full h-full bg-green-500
-
-// h-full w-full bg-white
